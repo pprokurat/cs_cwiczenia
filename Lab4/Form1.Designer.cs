d458339a2d3a4932adaf592b368f5dc1194a1354
Patryk Prokurat <patryk_prokurat@op.pl>
2018-06-18 21:34:10 +0200

    Lab 4 - rysowanie linii i prostokątów

---
+++ Lab4/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(3, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(477, 318);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            this.panel1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDoubleClick);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Line",
+            "Rectangle",
+            "Custom"});
+            this.listBox1.Location = new System.Drawing.Point(486, 31);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "Black",
+            "Red",
+            "Blue",
+            "Green"});
+            this.listBox2.Location = new System.Drawing.Point(487, 155);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 2;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 321);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ Lab4/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        public Point current = new Point();
+        public Point old = new Point();
+        int firstRect, secondRect;
+        public Graphics g;
+        public Pen pen = new Pen(Color.Black, 5);
+        public Color paintcolor = Color.Black;
+        bool draw;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = panel1.CreateGraphics();
+        }           
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+
+
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (draw)
+            {
+                if (listBox1.SelectedIndex == 0 && e.Button == MouseButtons.Left)
+                {
+                    current = e.Location;
+
+                    g.DrawLine(pen, old, current);
+
+                    old = current;
+                }
+                else if (listBox1.SelectedIndex == 1)
+                {
+                    if (e.X >= firstRect && e.Y >= secondRect)
+                        g.FillRectangle(new SolidBrush(paintcolor), firstRect, secondRect, e.X - firstRect, e.Y - secondRect);
+                }
+            }            
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            old = e.Location;
+
+            firstRect = e.X;
+            secondRect = e.Y;
+
+            draw = true;
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            draw = false;
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedIndex == 0)
+            {
+                pen = new Pen(Color.Black, 5);
+                paintcolor = Color.Black;
+            }
+            else if (listBox2.SelectedIndex == 1)
+            {
+                pen = new Pen(Color.Red, 5);
+                paintcolor = Color.Red;
+            }
+            else if (listBox2.SelectedIndex == 2)
+            {
+                pen = new Pen(Color.Blue, 5);
+                paintcolor = Color.Blue;
+            }
+            else if (listBox2.SelectedIndex == 3)
+            {
+                pen = new Pen(Color.Green, 5);
+                paintcolor = Color.Green;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
